86ad8c86e950068ad3561650b0f48d77708403ab
Carlosw5 <142521048+Carlosw5@users.noreply.github.com>
2024-02-27 11:04:24 -0500

    Update musiccc.py

--- musiccc.py
+++ musiccc.py
@@ -4,11 +4,10 @@ from earsketch import *
 init()
 setTempo(120)
 
-sounds = [KHALID_NORM_BASS_808, KHALID_NORM_GUITAR_4, KHALID_NORM_DRUMBEAT]
+sounds = [ELECTRO_DRUM_MAIN_LOOPPART_038, RD_EDM_CHORDPART_3]
 for i in range(len(sounds)):
     sound = sounds[i]
     track = i+1
     fitMedia(sound, track, 1, 17)
-
 
 finish()
